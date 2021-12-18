fa95fd1153359ee092c6bfb3fec9e25ae1db76ca
Anuprasad <anug0g6@gmail.com>
2021-12-19 02:22:49 +0530

    checkout page modified and add place order

--- app_base/admin.py
+++ app_base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Products
+from .models import Order, Products
 # Register your models here.
 
 admin.site.register(Products)
+admin.site.register(Order)
--- app_base/models.py
+++ app_base/models.py
@@ -9,3 +9,12 @@ class Products(models.Model):
     discound_price = models.FloatField()
     category = models.CharField(max_length=100)
     image = models.CharField(max_length=500)
+
+class Order(models.Model):
+    items = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=1000)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
