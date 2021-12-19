f66d9b5fee5673b1e96333627c44d05555a88320
Anuprasad <anug0g6@gmail.com>
2021-12-19 19:55:55 +0530

    totel price and place order added

--- app_base/admin.py
+++ app_base/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Order, Products
 # Register your models here.
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "Outfit-Hub"
+admin.site.index_title = "Manage Outfit-Hub"
 
-admin.site.register(Products)
-admin.site.register(Order)
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+
+    
+    list_display = ('title','discription','original_price','discound_price','category')
+    search_fields = ('title','original_price','discound_price','category')
+    change_category_to_default.short_description ="Default Category"
+    actions = ('change_category_to_default',)
+    list_editable = ('original_price','discound_price','category')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('items','name','email','address','city','state','zipcode','total')
+    search_fields = ('name',)
+
+
+admin.site.register(Products,ProductAdmin)
+admin.site.register(Order,OrderAdmin)
--- app_base/models.py
+++ app_base/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Products(models.Model):
+
+    #def __self__(self):
+        #return self.title
     title = models.CharField(max_length=200)
     discription = models.TextField(max_length=500)
     original_price = models.FloatField()
@@ -18,3 +21,4 @@ class Order(models.Model):
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=200)
     zipcode = models.CharField(max_length=200)
+    total = models.CharField(max_length=200)
