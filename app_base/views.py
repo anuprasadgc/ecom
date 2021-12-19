3eedffded26593fd1c537ce4bc8e48945f89635a
Anuprasad <anug0g6@gmail.com>
2021-12-19 23:08:02 +0530

    added some design to cart

--- app_base/views.py
+++ app_base/views.py
@@ -13,9 +13,9 @@ def index(request):
         product_objects = product_objects.filter(title__icontains=item_name)
 
     #paginator
-    paginator = Paginator(product_objects,4)
+    """paginator = Paginator(product_objects,4)
     page = request.GET.get('page')
-    product_objects = paginator.get_page(page)
+    product_objects = paginator.get_page(page)"""
 
     return render(request,'app_base/home.html',{'product_objects':product_objects})
 
