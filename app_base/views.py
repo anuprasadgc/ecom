afa9b9c8c8d78ec32205b3a7c2ce030f2a330d6f
Anuprasad <anug0g6@gmail.com>
2021-12-20 17:39:34 +0530

    modified cart and place order

--- app_base/views.py
+++ app_base/views.py
@@ -10,18 +10,20 @@ def index(request):
     #search
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
-        product_objects = product_objects.filter(title__icontains=item_name)
-
+        product_objects = product_objects.filter(title__icontains=item_name)  
+        
     #paginator
     """paginator = Paginator(product_objects,4)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)"""
-
     return render(request,'app_base/home.html',{'product_objects':product_objects})
 
 def detail(request,id):
     product_object = Products.objects.get(id=id)
     return render(request, 'app_base/detail.html',{'product_object':product_object})
+
+def cart(request):
+    return render(request, 'app_base/cart.html')
     
 def checkout(request):
 
